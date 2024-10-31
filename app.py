75708cc57e17966dadb4afc283fe22422fc1de6d
Cody Roberson <codyaroberson@outlook.com>
2024-10-31 12:37:12 -0700

    added exception handling implemented synth ctrl
    implemented synth
    
    impoving things
    
    added exception handling

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import render_template
 
 import transceiver321.transceiver_serialdriver as ts
 transceiver = ts.Transceiver("/dev/ARDUINOATTENUATOR")
-
+import libvalon.valon5009 as valon
+synth = valon.Synthesizer("/dev/SYNTHESIZER")
 app = Flask(__name__)
 
 @app.route('/')
@@ -26,10 +27,37 @@ def handle_attenuation():
         print(transceiver.set_atten(2, rfin))
     except ValueError:
         return "BAD FORMAT", 400
-    print(rfout)
-    print(rfin)
+    except KeyError:
+        return "Unexpected Parameter", 400
     return 'OK'
 
 @app.route('/synth/', methods=["POST"]) 
 def handle_synthesizer():
-    return "not implemented yet, hold on"
+    if request.json is None:
+        return "EMPTY DATA", 400
+    try:
+        req = request.json
+        f = float(req['frequency'])
+        x = req['fakedata']
+        print(type(x))
+        w = float(req['power'])
+
+        if f < 20.0:
+            return "FREQUENCY TOO LOW", 400
+        if w < -15.0:
+            return "POWER SETTING TOO LOW", 400
+        if w > 15.0:
+            return "POWER SETTING TOO HIGH", 400
+
+        synth.set_frequency(2, f)
+        synth.set_rf_level(2, w)
+    except ValueError:
+        return "Not a Number", 400
+    except KeyError:
+        return "Unexpected Parameter", 400
+ 
+    except Exception as e:
+        print(e)
+        raise e
+
+    return "OK"
--- static/ui.js
+++ static/ui.js
@@ -4,7 +4,11 @@ const successModal = document.getElementById("successModal");
 const rfout_element = document.getElementById('rfout_atten_val');
 const rfin_element  = document.getElementById('rfin_atten_val');
 const btn_setatten = document.getElementById('btn_setatten')
+const btn_setsynth = document.getElementById('btn_setsynth')
+const synth_power_elem = document.getElementById('synthesizer-power')
+const synth_freq_elem = document.getElementById('synthesizer-frequency')
 
+//attech enter key event listeners to each box
 rfout_element.addEventListener("keypress", function(event){
   if (event.key === "Enter"){
     event.preventDefault();
@@ -17,8 +21,27 @@ rfin_element.addEventListener("keypress", function(event){
     btn_setatten.click();
   }
 });
+synth_freq_elem.addEventListener("keypress", function(event){
+  if (event.key === "Enter"){
+      event.preventDefault();
+      btn_setsynth.click();
+    }
+  });
+
+
+synth_power_elem.addEventListener("keypress", function(event){
+  if (event.key === "Enter"){
+      event.preventDefault();
+      btn_setsynth.click();
+    }
+  });
+
 btn_setatten.onclick = function(){
   setAttenuatorValues();
+};
+
+btn_setsynth.onclick = function(){
+  setValon();
 };
 
 function showModal(modalId) {
@@ -53,13 +76,13 @@ function setAttenuatorValues(){
   var rfin = parseFloat(rfin_element.value);
   if (!(rfout >= 0 && rfout <= 31.75)){
     rfout_element.value = "";
-    document.getElementById("YOUFUCKEDUP").innerText = "SET THE RFOUT TO BETWEEN 0 AND 31.75 YAH DINGUS";
+    document.getElementById("failure_message").innerText = "Please set the RFOUT/DRIVE attenuation to between 0 and 31.75";
     showModal("failModal");
     return;
   }
   if (!(rfin >= 0 && rfin <= 31.75)){
     rfin_element.value = "";
-    document.getElementById("YOUFUCKEDUP").innerText = "COMMOOOOON IS IT THAT HARD TO COUNT BETWEEN 0 AND 31.75";
+    document.getElementById("failure_message").innerText = "Please set the RFIN/SENSE attenuation to between 0 AND 31.75";
     showModal("failModal");
     return;
   }
